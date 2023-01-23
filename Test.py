5824769f86c5191c525b9ccfb5a9e372cc6e3e56
rajlds <rajlds@gmail.com>
2023-01-22 20:13:12 -0500

    new code

--- Test.py
+++ Test.py
@@ -1,10 +1,5 @@
-# Creating a class and object :
-class Dog:
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age
-        
-dog1 = Dog("Tommy",2)
-print("Name :",dog1.name )
-print("Age:",dog1.age)
+i = 1
+while i < 6:
+    print(i)
+    i += 1
 
