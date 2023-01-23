4e44804a96b5e3edf6ea0cde834071f085e316d9
rajlds <rajlds@gmail.com>
2023-01-22 20:11:09 -0500

    new code

--- Test.py
+++ Test.py
@@ -1,2 +1,10 @@
-for i in range(1,6):
-    print(i)
+# Creating a class and object :
+class Dog:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        
+dog1 = Dog("Tommy",2)
+print("Name :",dog1.name )
+print("Age:",dog1.age)
+
